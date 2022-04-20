3db0e47c989f5a1f592706420f551e525942ad8f
Sifat <ibnsifat@gmail.com>
2022-04-20 17:53:43 +0600

    individual responsive

--- src/Components/ProductsPages/Onboarding/Onboarding.js
+++ src/Components/ProductsPages/Onboarding/Onboarding.js
@@ -14,6 +14,7 @@ import onb8 from '../../images/onb8.png'
 import onb9 from '../../images/onb9.png'
 import check from '../../images/check.png'
 import './onboard.css'
+import Companies from '../../Common/Companies'
 export default function Onboarding() {
     const [flag, setFlag] = useState(true)
     const [flag2, setFlag2] = useState(false)
@@ -42,31 +43,30 @@ export default function Onboarding() {
             <Container className='my-5' data-aos="fade-up-left">
                 <section className='text-center '>
                     <h2 className='fs-1 fw-bold'>Onboard your new software engineers. <br />Upskill the ones you already have.</h2>
-                    <h3 className=' mt-5 '>Onboarding your growing team of developers is monotonous, <br />difficult, and time-consuming. It doesn't have to be.</h3>
+                    <h3 className=' mt-5 smallText'>Onboarding your growing team of developers is monotonous, <br />difficult, and time-consuming. It doesn't have to be.</h3>
                     <div className=" mt-3   ">
-                        <Button className='p-3 m-3 w-25' href="#">Request a demo</Button>
+                        <Button className='p-3 m-3  bluebtn' href="#">Request a demo</Button>
                     </div>
                     <div >
-                        <img className='img-fluid w-50' src={onb0} alt="" />
+                        <img className='img-fluid ' src={onb0} alt="" />
 
                     </div>
-                    <img className='img-fluid  w-75 my-5' src={onb1} alt="" />
-
+                    <Companies />
                 </section>
 
             </Container>
             {/* How we help */}
             <Container fluid data-aos="fade-up" className='text-center py-5 middle'>
-                <Container className='my-5 w-75'>
-                    <div className="bg-white mx-auto py-4 w-50 my-5 rounded-pill d-flex justify-content-around">
-                        <Button onClick={() => handleOnb('onb')} variant="outline-light onbButton  py-3 px-5 rounded-pill ">Onboard</Button>
-                        <Button onClick={() => handleOnb('up')} variant="outline-light onbButton  py-3 px-5 rounded-pill ">Upskill</Button>
-                        <Button onClick={() => handleOnb('retain')} variant="outline-light onbButton  py-3 px-5 rounded-pill">Retain</Button>
+                <Container className='my-5 onboarding75'>
+                    <div className="bg-white smallWidth  onboarding50 mx-auto py-4  my-5 rounded-pill d-flex justify-content-around">
+                        <Button onClick={() => handleOnb('onb')} variant="outline-light onbButton  py-3 onboardingPad  rounded-pill ">Onboard</Button>
+                        <Button onClick={() => handleOnb('up')} variant="outline-light onbButton  py-3  onboardingPad rounded-pill ">Upskill</Button>
+                        <Button onClick={() => handleOnb('retain')} variant="outline-light onbButton  py-3 onboardingPad rounded-pill">Retain</Button>
                     </div>
                     <Row className={flag ? ` my-5 demo` : `d-none my-5 demo`} >
-                        <Col >
+                        <Col xs={12} md={6}>
                             <img className='img-fluid' src={onb7} alt="" /></Col>
-                        <Col className='text-start mt-5 mx-0'>
+                        <Col xs={12} md={6} className='text-start mt-5 mx-0'>
                             <h2>Accelerate Engineer Learning</h2>
                             <p>Onboarding engineers doesn't have to be haphazard, unscalable, and time-consuming. Educative helps you code, ship, and scale your teams faster and farther than ever before.</p>
                             <div className="text-start">
@@ -78,9 +78,9 @@ export default function Onboarding() {
                         </Col>
                     </Row>
                     <Row className={flag2 ? ` my-5 demo` : `d-none my-5 demo`}>
-                        <Col >
+                        <Col xs={12} md={6}>
                             <img className='img-fluid' src={onb8} alt="" /></Col>
-                        <Col className='text-start mt-5 mx-0'>
+                        <Col xs={12} md={6} className='text-start mt-5 mx-0'>
                             <h2>Upskill your engineering team</h2>
                             <p>54% of engineers say not offering resources to drive confidence in work is unappealing. Educative offers active-learning opportunities for even the most seasoned developers. Hands-on, interactive training ensures everyone learns exactly what they need.</p>
                             <div className="text-start">
@@ -93,9 +93,9 @@ export default function Onboarding() {
                         </Col>
                     </Row>
                     <Row className={flag3 ? ` my-5 demo` : `d-none my-5 demo`}>
-                        <Col >
+                        <Col xs={12} md={6}>
                             <img className='img-fluid' src={onb9} alt="" /></Col>
-                        <Col className='text-start mt-5 mx-0'>
+                        <Col xs={12} md={6} className='text-start mt-5 mx-0'>
                             <h2>Retain your best engineers</h2>
                             <p>74% of the engineers are actively looking for new opportunities and consider opportunity to learn as being an important factor when comparing current to future employers.</p>
                             <div className="text-start">
@@ -116,10 +116,10 @@ export default function Onboarding() {
                 <section className='container my-5 '>
 
                     <Row>
-                        <Col xs={6}>
+                        <Col xs={12} md={6}>
                             <img className='img-fluid' src={onb2} alt="" />
                         </Col>
-                        <Col xs={6} className="text-start pt-3">
+                        <Col xs={12} md={6} className="text-start pt-3">
                             <h3 className='fs-4 fw-bold'>Custom Onboarding Plans</h3>
                             <p className='text-start fs-5'>Engineering teams are like fingerprints - no two are exactly alike. Tailor unique paths for teams so new hires fill their knowledge gaps and ramp on broader technology, your products, and internal processes. Include in your custom plans things like:</p>
                             <div className="text-start">
@@ -131,20 +131,20 @@ export default function Onboarding() {
                         </Col>
                     </Row>
                     <Row className='my-5 py-2'>
-                        <Col xs={6} className="text-start pt-5">
+                        <Col xs={12} md={6} className="text-start pt-5">
                             <h1 className='fw-bold'>Educative Courses Integration</h1>
                             <p className='text-start fs-4 pt-5'>Need to teach a new language or technology? Want to knock the rust off an established skill? Integrate your onboarding with the leading provider of hands-on interactive coding so your software engineers grow faster and farther.</p>
 
                         </Col>
-                        <Col xs={6}>
+                        <Col xs={12} md={6}>
                             <img className='mt-3 w-75' src={bus4} alt="" />
                         </Col>
                     </Row>
                     <Row>
-                        <Col xs={6}>
+                        <Col xs={12} md={6}>
                             <img className='mt-5 w-75' src={onb3} alt="" />
                         </Col>
-                        <Col xs={6} className="text-start">
+                        <Col xs={12} md={6} className="text-start">
                             <h3 className='fw-bold mt-5 pt-5'>Assign Mentors</h3>
                             <p className='text-start fs-4 pt-3'>New hires can quickly get overwhelmed during Onboarding and are unaware of whom to reach out to for help. Enhance their onboarding experience by assigning them Mentors.</p>
 
@@ -152,20 +152,20 @@ export default function Onboarding() {
 
                     </Row>
                     <Row className='my-5 py-2'>
-                        <Col xs={6} className="text-start pt-5">
+                        <Col xs={12} md={6} className="text-start pt-5">
                             <h1 className='fw-bold'>Unblock your mentees</h1>
                             <p className='text-start fs-4 pt-5'>Jump in when help is needed. Stay out of the way when it’s not. Mentees can raise their virtual hand to signal their mentor when they’re stuck. Notifications alert the mentor upon login to answer the question and get the mentee back on track.</p>
 
                         </Col>
-                        <Col xs={6}>
+                        <Col xs={12} md={6}>
                             <img className='mt-3 w-75' src={onb4} alt="" />
                         </Col>
                     </Row>
                     <Row>
-                        <Col xs={6}>
+                        <Col xs={12} md={6}>
                             <img className='mt-5 w-75' src={onb5} alt="" />
                         </Col>
-                        <Col xs={6} className="text-start">
+                        <Col xs={12} md={6} className="text-start">
                             <h3 className='fw-bold mt-5 pt-5'>Capture Actionable Insights</h3>
                             <p className='text-start fs-4 pt-3'>Oversee the progress of your new hires, the performance of your mentors, and the overall efficiency of your Onboarding program, with our reports and dashboards.</p>
 
@@ -173,12 +173,12 @@ export default function Onboarding() {
 
                     </Row>
                     <Row className='my-5 py-2'>
-                        <Col xs={6} className="text-start pt-5">
+                        <Col xs={12} md={6} className="text-start pt-5">
                             <h1 className='fw-bold'>White Glove Service</h1>
                             <p className='text-start fs-4 pt-5'>Migrate your existing content and assets from anywhere to Onboarding or let Educative create a tailored Onboarding program from scratch just for you.</p>
 
                         </Col>
-                        <Col xs={6}>
+                        <Col xs={12} md={6}>
                             <img className='mt-3 w-75' src={onb6} alt="" />
                         </Col>
                     </Row>
--- src/Components/ProductsPages/Upskill/Upskill.js
+++ src/Components/ProductsPages/Upskill/Upskill.js
@@ -17,10 +17,10 @@ export default function Upskill() {
                     <p className=' mt-5 '>Educative helps engineering managers and their teams learn, practice,<br />and test their coding skills using hands-on, interactive tools right from their internet browser.</p>
                     <p className=' '></p>
                     <div className=" mt-3   ">
-                        <Button className='p-3 m-3 w-25' href="#">Request a demo</Button>
+                        <Button className='p-3 m-3 bluebtn' href="#">Request a demo</Button>
                     </div>
                     <div >
-                        <img className='img-fluid w-50' src={up0} alt="" />
+                        <img className='img-fluid ' src={up0} alt="" />
 
                     </div>
                 </section>
@@ -44,43 +44,42 @@ export default function Upskill() {
 
             {/* Custom leaerning */}
             <Container fluid data-aos="fade-up-right" className='text-center py-5 '>
-                {/* middle part */}
                 <section className='container my-5 '>
 
                     <Row>
-                        <Col xs={6} className="text-start pt-5">
+                        <Col xs={12} md={6} className="text-start pt-5">
                             <h1 className='fw-bold'>Custom Learning Paths</h1>
                             <p className='text-start fs-4 pt-5'>No two engineers are exactly the same, so why is their curriculum? Educative helps you customize their training and paths so each engineer on your team gets exactly what they need and skip what they've already mastered.</p>
 
                         </Col>
-                        <Col xs={6}>
+                        <Col xs={12} md={6}>
                             <img className='mt-3 w-75' src={up1} alt="" />
                         </Col>
                     </Row>
                     <Row className='my-5 py-2'>
-                        <Col xs={6}>
+                        <Col xs={12} md={6}>
                             <img className='img-fluid' src={up2} alt="" />
                         </Col>
-                        <Col xs={6} className="text-start pt-3">
+                        <Col xs={12} md={6} className="text-start pt-3">
                             <h3 className='fs-4 fw-bold'>Active Participation</h3>
                             <p className='text-start fs-5'>Watching videos is great for the weekend but hands-on, in-browser practice ensures that engineers get real-time feedback. Active learning increases retention and ensures that engineers spend more time coding and less time watching.</p>
 
                         </Col>
                     </Row>
                     <Row>
-                        <Col xs={6} className="text-start">
+                        <Col xs={12} md={6} className="text-start">
                             <h3 className='fw-bold'>Engagement Tracking</h3>
                             <p className='text-start fs-4 pt-3'>Educative empowers you with insights into your team's learning— who is taking the courses, and more importantly, who is not. Celebrate your team's progress and remind passive participants to accept your invitation to grow, learn and improve using the Educative platform.</p>
 
                         </Col>
-                        <Col xs={6}>
+                        <Col xs={12} md={6}>
                             <img className='mt-5 w-75' src={up3} alt="" />
                         </Col>
                     </Row>
                 </section>
 
             </Container>
-
+            {/* demo */}
             <Container fluid data-aos="fade-up" className='text-center py-5 middle'>
 
                 <Demo />
