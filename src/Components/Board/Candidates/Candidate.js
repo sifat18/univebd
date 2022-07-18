9bc5646a2c6771c3289bf5c13644df232be5c79a
sifat18 <ibnsifat@gmail.com>
2022-07-18 16:31:17 +0600

    filter

--- src/Components/Board/Candidates/Candidate.js
+++ src/Components/Board/Candidates/Candidate.js
@@ -4,29 +4,39 @@ import { Button, Container, Table } from 'react-bootstrap'
 export default function 
 Candidate() {
     const [user, setUser] = useState([])
+    const [filter, setFilterData] = useState([])
     const [position, setPosition] = useState('')
+    const [sl, setSl] = useState('')
     const [skills, setSkills] = useState([])
     useEffect(() => {
         fetch(`https://fierce-woodland-01411.herokuapp.com/resume`).then(res => res.json()).then(data => {
             setUser(data)
-            data.map((m, idx)=> (
-                setSkills([...skills,...m.skills.frameworks, ...m.skills.databases,...m.skills.technologies,...m.skills.tools])
-            ))
-        })
+           setFilterData(data)
+           data.map((m, idx)=> (
+            setSkills([...skills,...m.skills.frameworks, ...m.skills.databases,...m.skills.technologies,...m.skills.tools])
+        ))
+            })
     }, [])
 
     const handsearch=()=>{
-        console.log(position)
-        const newData=user.filter(x=>x.basics.label==position)
+        console.log(sl)
+        const newData=user.filter(x=>x.basics.label===position)
+        // .filter(x=>x.basics.label==(position==''?x.basics.label:position))
+        // .filter(y=>y.skills.frameworks.filter(z=>z.name==(sl==''?z.name:sl)))
         setUser(newData)
         console.log(newData)
-    }
+        }
+        // .filter(x=>x.basics.label==position)
+        // .filter(x=>x.skills==sl)
+        // console.log(newData)
+    
 
     return (
         <Container>
             <Table>
                 <tr>
                     <td><input type="text" placeholder='...enter position' onChange={(e)=>setPosition(e.target.value)}/></td>
+                    {/* <td><input type="text" placeholder='...enter skill' onChange={(e)=>setSl(e.target.value)}/></td> */}
                     <td><Button onClick={handsearch}>search</Button></td>
 
                 </tr>
@@ -49,7 +59,7 @@ Candidate() {
                             <td>{m.basics?.name}</td>
                             <td>{m.basics?.email}</td>
                             <td>{m.basics?.label}</td>
-                            <td>{skills.map(x=>x.name+', ')}</td>
+                            <td>{m.skills.frameworks.map(x=>x.name+', ')+m.skills.databases.map(x=>x.name+', ')+ m.skills.technologies.map(x=>x.name+', ')+m.skills.tools.map(x=>x.name+', ')}</td>
                             <td><Button variant='primary'>View details</Button></td>
                         </tr>
                     ))}
