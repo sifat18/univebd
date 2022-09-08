13bc974934024feb4d74cae1d6155f486ae0ec7b
sifat18 <ibnsifat@gmail.com>
2022-09-08 11:45:12 +0600

    update icons

--- src/Components/Board/AppSidebar/AppSidebarNav.js
+++ src/Components/Board/AppSidebar/AppSidebarNav.js
@@ -19,8 +19,15 @@ import {
 // import { CNavItem, CNavTitle } from '@coreui/react'
 import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 import useAuth from '../../Context/useAuth'
-import { RiMapPinUserLine } from 'react-icons/ri'
+import { RiAdminLine, RiMapPinUserLine } from 'react-icons/ri'
 import { Container } from 'react-bootstrap'
+import { MdOutlineAdminPanelSettings, MdOutlineComputer } from 'react-icons/md'
+import { FaChalkboardTeacher } from 'react-icons/fa'
+import { IoCreateOutline } from 'react-icons/io5'
+import { BsFillInboxFill, BsPersonBadge, BsPersonCheck } from 'react-icons/bs'
+import { GrFormFolder } from 'react-icons/gr'
+import { BiUserCircle } from 'react-icons/bi'
+import { ImProfile } from 'react-icons/im'
 
 export const AppSidebarNav = () => {
   const { user, admin, employer } = useAuth()
@@ -39,53 +46,53 @@ export const AppSidebarNav = () => {
       name: 'Navigate',
     }, {
       component: CNavGroup,
-      name: 'Admin Routes',
+      name: ' Admin Routes',
       to: '/dashboard',
-      icon: <CIcon icon={cilCursor} customClassName="nav-icon text-dark" />,
+      icon: <MdOutlineAdminPanelSettings  customClassName="nav-icon fs-2 text-dark" />,
       items: [
 
         {
           component: CNavItem,
-          name: 'Make New Admin',
+          name: ' Make New Admin',
           to: '/dashboard/adadmin',
-          icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+          icon: <RiAdminLine customClassName="nav-icon  text-dark" />,
         },
         {
           component: CNavItem,
           name: 'Make New Tutor',
           to: '/dashboard/adtutor',
-          icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+          icon: <FaChalkboardTeacher  customClassName="nav-icon text-dark" />,
         },
         {
           component: CNavItem,
           name: 'Make New Employer',
           to: '/dashboard/ademployer',
-          icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+          icon: <BsPersonCheck customClassName="nav-icon text-dark" />,
         },
 
         {
           component: CNavItem,
           name: 'Create New Course',
           to: '/dashboard/addcourse',
-          icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+          icon: <BsFillInboxFill customClassName="nav-icon text-dark" />,
         },
         {
           component: CNavItem,
           name: 'Available Candidates',
           to: '/dashboard/candidates',
-          icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+          icon: <BsPersonBadge customClassName="nav-icon text-dark" />,
         },
         {
           component: CNavItem,
           name: 'Form Management',
           to: '/dashboard/form_management',
-          icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+          icon: <GrFormFolder  customClassName="nav-icon text-dark" />,
         },
         {
           component: CNavItem,
           name: 'Create Employer Profile',
           to: '/dashboard/employer_profile',
-          icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+          icon: <IoCreateOutline customClassName="nav-icon text-dark" />,
         },
       ]
     },
@@ -93,19 +100,19 @@ export const AppSidebarNav = () => {
       component: CNavGroup,
       name: 'User Routes',
       to: '/dashboard',
-      icon: <CIcon icon={cilCursor} customClassName="nav-icon text-dark" />,
+      icon: <BiUserCircle  customClassName="nav-icon text-dark" />,
       items: [
         {
           component: CNavItem,
           name: 'Profile Setting',
           to: `/dashboard/update_profile`,
-          icon: <CIcon icon={cilDrop} customClassName="nav-icon text-dark" />,
+          icon: <ImProfile customClassName="nav-icon text-dark" />,
         },
         {
           component: CNavItem,
           name: 'My Courses',
           to: `/dashboard/mycourses`,
-          icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+          icon: <MdOutlineComputer  customClassName="nav-icon text-dark" />,
         },
       ],
     },
@@ -126,7 +133,13 @@ export const AppSidebarNav = () => {
       component: CNavItem,
       name: 'Create Employer Profile',
       to: '/dashboard/employer_profile',
-      icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+      icon: <IoCreateOutline customClassName="nav-icon text-dark" />,
+    },
+    {
+      component: CNavItem,
+      name: 'Available Candidates',
+      to: '/dashboard/candidates',
+      icon: <BsPersonBadge customClassName="nav-icon text-dark" />,
     },
 
   ]
@@ -146,249 +159,24 @@ export const AppSidebarNav = () => {
       component: CNavItem,
       name: 'Profile Setting',
       to: `/dashboard/update_profile`,
-      icon: <CIcon icon={cilDrop} customClassName="nav-icon text-dark" />,
+      icon: <BiUserCircle customClassName="nav-icon text-dark" />,
     },
     {
       component: CNavItem,
       name: 'My Courses',
       to: `/dashboard/mycourses`,
-      icon: <CIcon icon={cilPencil} customClassName="nav-icon text-dark" />,
+      icon: <MdOutlineComputer customClassName="nav-icon text-dark" />,
     },
-    // {
-    //   component: CNavTitle,
-    //   name: 'Components',
-    // },
-    // {
-    //   component: CNavGroup,
-    //   name: 'Base',
-    //   to: '/dashboard/base',
-    //   icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
-    //   items: [
-    //     {
-    //       component: CNavItem,
-    //       name: 'Accordion',
-    //       to: '/dashboard/base/accordion',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Breadcrumb',
-    //       to: '/dashboard/base/breadcrumbs',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Cards',
-    //       to: '/dashboard/base/cards',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Carousel',
-    //       to: '/dashboard/base/carousels',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Collapse',
-    //       to: '/dashboard/base/collapses',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'List group',
-    //       to: '/dashboard/base/list-groups',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Navs & Tabs',
-    //       to: '/dashboard/base/navs',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Pagination',
-    //       to: '/dashboard/base/paginations',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Placeholders',
-    //       to: '/dashboard/base/placeholders',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Popovers',
-    //       to: '/dashboard/base/popovers',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Progress',
-    //       to: '/dashboard/base/progress',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Spinners',
-    //       to: '/dashboard/base/spinners',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Tables',
-    //       to: '/dashboard/base/tables',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Tooltips',
-    //       to: '/dashboard/base/tooltips',
-    //     },
-    //   ],
-    // },
-    // {
-    //   component: CNavGroup,
-    //   name: 'Buttons',
-    //   to: '/dashboard/buttons',
-    //   icon: <CIcon icon={cilCursor} customClassName="nav-icon" />,
-    //   items: [
-    //     {
-    //       component: CNavItem,
-    //       name: 'Buttons',
-    //       to: '/dashboard/buttons/buttons',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Buttons groups',
-    //       to: '/dashboard/buttons/button-groups',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Dropdowns',
-    //       to: '/dashboard/buttons/dropdowns',
-    //     },
-    //   ],
-    // },
-    // {
-    //   component: CNavGroup,
-    //   name: 'Forms',
-    //   icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
-    //   items: [
-    //     {
-    //       component: CNavItem,
-    //       name: 'Form Control',
-    //       to: '/dashboard/forms/form-control',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Select',
-    //       to: '/dashboard/forms/select',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Checks & Radios',
-    //       to: '/dashboard/forms/checks-radios',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Range',
-    //       to: '/dashboard/forms/range',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Input Group',
-    //       to: '/dashboard/forms/input-group',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Floating Labels',
-    //       to: '/dashboard/forms/floating-labels',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Layout',
-    //       to: '/dashboard/forms/layout',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Validation',
-    //       to: '/dashboard/forms/validation',
-    //     },
-    //   ],
-    // },
-    // {
-    //   component: CNavItem,
-    //   name: 'Charts',
-    //   to: '/dashboard/charts',
-    //   icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
-    // },
-    // {
-    //   component: CNavGroup,
-    //   name: 'Icons',
-    //   icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
-    //   items: [
-    //     {
-    //       component: CNavItem,
-    //       name: 'CoreUI Free',
-    //       to: '/dashboard/icons/coreui-icons',
-    //       badge: {
-    //         color: 'success',
-    //         text: 'NEW',
-    //       },
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'CoreUI Flags',
-    //       to: '/dashboard/icons/flags',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'CoreUI Brands',
-    //       to: '/dashboard/icons/brands',
-    //     },
-    //   ],
-    // },
-    // {
-    //   component: CNavGroup,
-    //   name: 'Notifications',
-    //   icon: <CIcon icon={cilBell} customClassName="nav-icon" />,
-    //   items: [
-    //     {
-    //       component: CNavItem,
-    //       name: 'Alerts',
-    //       to: '/dashboard/notifications/alerts',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Badges',
-    //       to: '/dashboard/notifications/badges',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Modal',
-    //       to: '/dashboard/notifications/modals',
-    //     },
-    //     {
-    //       component: CNavItem,
-    //       name: 'Toasts',
-    //       to: '/dashboard/notifications/toasts',
-    //     },
-    //   ],
-    // },
-    // {
-    //   component: CNavItem,
-    //   name: 'Widgets',
-    //   to: '/dashboard/widgets',
-    //   icon: <CIcon icon={cilCalculator} customClassName="nav-icon" />,
-    //   badge: {
-    //     color: 'info',
-    //     text: 'NEW',
-    //   },
-    // },
-    // {
-    //   component: CNavTitle,
-    //   name: 'Extras',
-    // },
-
+   
   ]
   const navLink = (name, icon, badge) => {
     return (
       <>
         <Container className="text-dark ">
-          {icon && icon}
+          {icon && <span className='fs-4 mleft'> {icon}</span>}
           {name && name}
           {badge && (
-            <CBadge className="ms-auto text-dark">
+            <CBadge className="ms-auto  text-dark">
               {badge.text}
             </CBadge>
           )}
@@ -401,7 +189,7 @@ export const AppSidebarNav = () => {
     const { component, name, badge, icon, ...rest } = item
     const Component = component
     return (
-      <Component className='text-dark ashHover ms-1 '
+      <Component className='text-dark  ashHover'
         {...(rest.to &&
           !rest.items && {
           component: NavLink,
