e5c03c9528ed87ddb9a72682676e280a47a3d0ed
sifat18 <ibnsifat@gmail.com>
2022-10-01 17:54:50 +0600

    update

--- src/Components/Common/JobPost.js
+++ src/Components/Common/JobPost.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Card, Col, Container, Row } from 'react-bootstrap'
 
-export default function JobPost() {
+export default function JobPost({job}) {
   return (
-    <div>JobPost</div>
+    <Container>
+       <Row xs={1} md={3} className="g-4 ">
+            <Col >
+            <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{job.position}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+            </Col>
+            
+        </Row>
+    </Container>
   )
 }
--- src/Components/JobBrowsing/JobBrowsing.js
+++ src/Components/JobBrowsing/JobBrowsing.js
@@ -5,6 +5,7 @@ import pic6 from '../images/logos/logo.png'
 import { Container } from 'react-bootstrap';
 import { useState } from 'react';
 import JobPost from '../Common/JobPost';
+import Header from './../Header/Header';
 export default function JobBrowsing() {
   const [jobs, setJobs] = useState([])
 useEffect(() => {
@@ -13,16 +14,17 @@ useEffect(() => {
 console.log(jobs)
     return (
     <>
-      <Container fluid className='job-bg'>
+    <Header/>
+      {/* <Container fluid className='job-bg'>
       <Container data-aos="fade-left" className='d-flex flex-column justify-content-center align-items-center  mt-5 pt-2'>
                 <img data-aos="fade-left" src={pic6} alt="Logo" className="logo-img img-fluid" height='250' width='250' />              
                 <h2 data-aos="fade-right" className='text-bg text-white'>Are you stressed about your career?</h2>
                 <p data-aos="fade-up" className='text-white fs-4 '>We are here to help you grow </p>
                 <p data-aos="fade-down" className='text-white fs-4 '>It's an all in one platform where you can learn and find your dream job</p>
             </Container>
-        </Container>  
+        </Container>   */}
 
-        <Container>
+        <Container >
 {jobs.map((job,index)=>{
     <JobPost key={index} job={job}/>
 })}
