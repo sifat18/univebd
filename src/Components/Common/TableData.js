b8e89f2cd708fd18b4bb7545bfd4d1a4bd564042
sifat18 <ibnsifat@gmail.com>
2022-10-06 17:48:19 +0600

    update form status

--- src/Components/Common/TableData.js
+++ src/Components/Common/TableData.js
@@ -1,9 +1,41 @@
 import React from 'react'
-import { Table } from 'react-bootstrap'
+import { Table,Form } from 'react-bootstrap'
+import { useState } from 'react';
+import { axios } from 'axios';
 
 export default function TableData({tableData,choice}) {
-  console.log(tableData)
-    return (
+  // const [status, setStatus] = useState('Change Status ');
+  function handleSelectChange(event,id,choice) {
+    // setStatus(event.target.value);
+    console.log(event.target.value)
+
+    switch(choice){
+      case 'contributer':
+        axios.put(`https://fierce-woodland-01411.herokuapp.com/api/contributer/edit/${id}?choice=${event.target.value}`).then(res => res.data ? console.log('dsane') : '')
+        break;
+      case 'instructor':
+        console.log('instructor')
+        axios.put(`https://fierce-woodland-01411.herokuapp.com/api/instructor/edit/${id}?choice=${event.target.value}`).then(res => res.data ? console.log('dsane') : '')
+        break;
+      case 'recruitement':
+        axios.put(`https://fierce-woodland-01411.herokuapp.com/api/unive_recruitement/edit/${id}?choice=${event.target.value}`).then(res => res.data ? console.log('dsane') : '')
+        break;
+      case 'representative':
+        axios.put(`https://fierce-woodland-01411.herokuapp.com/api/representative/edit/${id}?choice=${event.target.value}`).then(res => res.data ? console.log('dsane') : '')
+        break;
+      case 'demo':
+        axios.put(`https://fierce-woodland-01411.herokuapp.com/api/demo/edit/${id}?choice=${event.target.value}`).then(res => res.data ? console.log('dsane') : '')
+        break;
+      case 'enterprice':
+        axios.put(`https://fierce-woodland-01411.herokuapp.com/api/enterprice/edit/${id}?choice=${event.target.value}`).then(res => res.data ? console.log('dsane') : '')
+        break;
+      case 'scholarship':
+        axios.put(`https://fierce-woodland-01411.herokuapp.com/api/scholarship/edit/${id}?choice=${event.target.value}`).then(res => res.data ? console.log('dsane') : '')
+        break;
+      default: 
+    }
+  }
+  return (
     
     <div>
          <Table responsive striped bordered hover>
@@ -22,6 +54,7 @@ export default function TableData({tableData,choice}) {
           {choice==="recruitement"  &&<th>Candidate Requirement</th>}
           {choice==="recruitement"  &&<th>Additional_information</th>}
           <th>Status</th>
+          <th>Status Change</th>
 
         </tr>
       </thead>
@@ -41,6 +74,12 @@ export default function TableData({tableData,choice}) {
           {choice==="scholarship"  &&<td>{m.scholarship_need}</td>}
           {choice==="scholarship"  &&<td>{m.platform_learn}</td>}
           <td className={m.status==="pending" && 'text-danger'}>{m.status}</td>
+          <td>
+            <Form.Select className='ms-2' aria-label="Type of Form:" name='form_type' onChange={handleSelectChange} >
+                <option>Choose status</option>
+                <option value="pending">pending</option>
+                <option value="Reviewed">Reviewed</option>
+              </Form.Select></td>
                       </tr>
                     ))}
       </tbody>
