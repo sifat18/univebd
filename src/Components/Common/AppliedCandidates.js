d7d4c9738d9d1a512a9b0a5ec2b96608a5f281ad
sifat18 <ibnsifat@gmail.com>
2022-10-12 16:23:21 +0600

    update pdf view

--- src/Components/Common/AppliedCandidates.js
+++ src/Components/Common/AppliedCandidates.js
@@ -1,9 +1,20 @@
 import { useEffect, useState } from 'react';
-import { Container, Row, Table } from 'react-bootstrap';
+import { Button, Container, Row, Table } from 'react-bootstrap';
 import JobPost from './JobPost';
+import SinglePagePdf from './SinglePagePdf';
 export default function AppliedCandidates() {
     const [jobs, setJobs] = useState([])
-    let ema='info@muspana.com'
+    const [showT, setShowT] = useState(false);
+    const [pdf, setPdf] = useState(null);
+    const handleView=view=>{
+      setPdf(view)
+      handleShowT()
+    }
+    const handleCloseT = () => {
+      setPdf(null)
+      setShowT(false)};
+    const handleShowT = () => setShowT(true)
+     let ema='info@muspana.com'
     useEffect(() => {
         fetch(`https://fierce-woodland-01411.herokuapp.com/api/employer_posted/${ema}`).then(res=>res.json()).then(data=>setJobs(data))
     }, [ema])
@@ -21,6 +32,14 @@ export default function AppliedCandidates() {
     }
   return (
     <>
+    {showT && <Container className='text-center'>
+             <h2> Candidate Resume </h2>
+             <hr/>
+             <SinglePagePdf pdf={"data:application/pdf;base64,"+pdf}/>
+                 <Button variant="success" onClick={handleCloseT}>
+                     Ok!!
+                 </Button>
+         </Container>}
     <Container className='mb-5'>
     <h2 className='my-4 text-center fw-bold my-5'> Browse  Available Jobs</h2>
    {content}
@@ -40,12 +59,14 @@ export default function AppliedCandidates() {
                         <td>{idx + 1}</td>
                         <td>{m.email}</td>
                         <td>{m.jobData.position}</td>
-                        <td>{m.jobData.pdf}</td>
+                        {console.log("data:application/pdf;base64,"+m.pdf)}
+                        <td><Button variant='primary' onClick={()=>handleView(m.pdf)}> View Resume</Button> </td>
+                        {/* <td><SinglePagePdf pdf={"data:application/pdf;base64,"+m.pdf}/></td> */}
                         </tr>
                     ))}
       </tbody>
       </Table>
-
+      
    </>
   )
 }
