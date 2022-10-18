8bee6d252204fe45b7bc8abcb944eacf68282e7e
sifat18 <ibnsifat@gmail.com>
2022-10-18 16:59:29 +0600

    dashboard profile section

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -16,134 +16,16 @@ import {
 // import WidgetsBrand from '../widgets/WidgetsBrand'
 // import WidgetsDropdown from '../widgets/WidgetsDropdown'
 import { useEffect, useState } from 'react'
-import { Container, Table } from 'react-bootstrap'
+import pic1 from '../../Components/images/icons8-multiply-16.png'
+import { Col, Container, Dropdown, Form, Row, Table } from 'react-bootstrap'
 import { HiOutlineMinus } from 'react-icons/hi'
+import { AiTwotoneStar } from 'react-icons/ai'
+import { BsPencilSquare } from 'react-icons/bs'
+import { ImCross } from 'react-icons/im'
 import useAuth from '../../Components/Context/useAuth'
 
 const Dashboard = () => {
   const { user,admin } = useAuth()
-  // const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
-
-  // const progressExample = [
-  //   { title: 'Visits', value: '29.703 Users', percent: 40, color: 'success' },
-  //   { title: 'Unique', value: '24.093 Users', percent: 20, color: 'info' },
-  //   { title: 'Pageviews', value: '78.706 Views', percent: 60, color: 'warning' },
-  //   { title: 'New Users', value: '22.123 Users', percent: 80, color: 'danger' },
-  //   { title: 'Bounce Rate', value: 'Average Rate', percent: 40.15, color: 'primary' },
-  // ]
-
-  // const progressGroupExample1 = [
-  //   { title: 'Monday', value1: 34, value2: 78 },
-  //   { title: 'Tuesday', value1: 56, value2: 94 },
-  //   { title: 'Wednesday', value1: 12, value2: 67 },
-  //   { title: 'Thursday', value1: 43, value2: 91 },
-  //   { title: 'Friday', value1: 22, value2: 73 },
-  //   { title: 'Saturday', value1: 53, value2: 82 },
-  //   { title: 'Sunday', value1: 9, value2: 69 },
-  // ]
-
-  // const progressGroupExample2 = [
-  //   { title: 'Male', icon: cilUser, value: 53 },
-  //   { title: 'Female', icon: cilUserFemale, value: 43 },
-  // ]
-
-  // const progressGroupExample3 = [
-  //   { title: 'Organic Search', icon: cibGoogle, percent: 56, value: '191,235' },
-  //   { title: 'Facebook', icon: cibFacebook, percent: 15, value: '51,223' },
-  //   { title: 'Twitter', icon: cibTwitter, percent: 11, value: '37,564' },
-  //   { title: 'LinkedIn', icon: cibLinkedin, percent: 8, value: '27,319' },
-  // ]
-
-  // const tableExample = [
-  //   {
-  //     avatar: { src: avatar1, status: 'success' },
-  //     user: {
-  //       name: 'Yiorgos Avraamu',
-  //       new: true,
-  //       registered: 'Jan 1, 2021',
-  //     },
-  //     country: { name: 'USA', flag: cifUs },
-  //     usage: {
-  //       value: 50,
-  //       period: 'Jun 11, 2021 - Jul 10, 2021',
-  //       color: 'success',
-  //     },
-  //     payment: { name: 'Mastercard', icon: cibCcMastercard },
-  //     activity: '10 sec ago',
-  //   },
-  //   {
-  //     avatar: { src: avatar2, status: 'danger' },
-  //     user: {
-  //       name: 'Avram Tarasios',
-  //       new: false,
-  //       registered: 'Jan 1, 2021',
-  //     },
-  //     country: { name: 'Brazil', flag: cifBr },
-  //     usage: {
-  //       value: 22,
-  //       period: 'Jun 11, 2021 - Jul 10, 2021',
-  //       color: 'info',
-  //     },
-  //     payment: { name: 'Visa', icon: cibCcVisa },
-  //     activity: '5 minutes ago',
-  //   },
-  //   {
-  //     avatar: { src: avatar3, status: 'warning' },
-  //     user: { name: 'Quintin Ed', new: true, registered: 'Jan 1, 2021' },
-  //     country: { name: 'India', flag: cifIn },
-  //     usage: {
-  //       value: 74,
-  //       period: 'Jun 11, 2021 - Jul 10, 2021',
-  //       color: 'warning',
-  //     },
-  //     payment: { name: 'Stripe', icon: cibCcStripe },
-  //     activity: '1 hour ago',
-  //   },
-  //   {
-  //     avatar: { src: avatar4, status: 'secondary' },
-  //     user: { name: 'Enéas Kwadwo', new: true, registered: 'Jan 1, 2021' },
-  //     country: { name: 'France', flag: cifFr },
-  //     usage: {
-  //       value: 98,
-  //       period: 'Jun 11, 2021 - Jul 10, 2021',
-  //       color: 'danger',
-  //     },
-  //     payment: { name: 'PayPal', icon: cibCcPaypal },
-  //     activity: 'Last month',
-  //   },
-  //   {
-  //     avatar: { src: avatar5, status: 'success' },
-  //     user: {
-  //       name: 'Agapetus Tadeáš',
-  //       new: true,
-  //       registered: 'Jan 1, 2021',
-  //     },
-  //     country: { name: 'Spain', flag: cifEs },
-  //     usage: {
-  //       value: 22,
-  //       period: 'Jun 11, 2021 - Jul 10, 2021',
-  //       color: 'primary',
-  //     },
-  //     payment: { name: 'Google Wallet', icon: cibCcApplePay },
-  //     activity: 'Last week',
-  //   },
-  //   {
-  //     avatar: { src: avatar6, status: 'danger' },
-  //     user: {
-  //       name: 'Friderik Dávid',
-  //       new: true,
-  //       registered: 'Jan 1, 2021',
-  //     },
-  //     country: { name: 'Poland', flag: cifPl },
-  //     usage: {
-  //       value: 43,
-  //       period: 'Jun 11, 2021 - Jul 10, 2021',
-  //       color: 'success',
-  //     },
-  //     payment: { name: 'Amex', icon: cibCcAmex },
-  //     activity: 'Last week',
-  //   },
-  // ]
 
   const [users, setUser] = useState([])
   const [totalUser, setTotalUser] = useState(0)
@@ -161,196 +43,14 @@ const Dashboard = () => {
         <CCardBody>
           <CRow>
             <CCol sm={5}>
-              <h4 id="traffic" className="card-title mb-0">
-                Traffic
-              </h4>
-              <div className="small text-medium-emphasis">January - July 2021</div>
-            </CCol>
-            <CCol sm={7} className="d-none d-md-block">
-              <CButton color="primary" className="float-end">
-                <CIcon icon={cilCloudDownload} />
-              </CButton>
-              <CButtonGroup className="float-end me-3">
-                {['Day', 'Month', 'Year'].map((value) => (
-                  <CButton
-                    color="outline-secondary"
-                    key={value}
-                    className="mx-0"
-                    active={value === 'Month'}
-                  >
-                    {value}
-                  </CButton>
-                ))}
-              </CButtonGroup>
-            </CCol>
-          </CRow>
-          <CChartLine
-            style={{ height: '300px', marginTop: '40px' }}
-            data={{
-              labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-              datasets: [
-                {
-                  label: 'My First dataset',
-                  // backgroundColor: hexToRgba(getStyle('--cui-info'), 10),
-                  borderColor: getStyle('--cui-info'),
-                  pointHoverBackgroundColor: getStyle('--cui-info'),
-                  borderWidth: 2,
-                  data: [
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                  ],
-                  fill: true,
-                },
-                {
-                  label: 'My Second dataset',
-                  backgroundColor: 'transparent',
-                  borderColor: getStyle('--cui-success'),
-                  pointHoverBackgroundColor: getStyle('--cui-success'),
-                  borderWidth: 2,
-                  data: [
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                    random(50, 200),
-                  ],
-                },
-                {
-                  label: 'My Third dataset',
-                  backgroundColor: 'transparent',
-                  borderColor: getStyle('--cui-danger'),
-                  pointHoverBackgroundColor: getStyle('--cui-danger'),
-                  borderWidth: 1,
-                  borderDash: [8, 5],
-                  data: [65, 65, 65, 65, 65, 65, 65],
-                },
-              ],
-            }}
-            options={{
-              maintainAspectRatio: false,
-              plugins: {
-                legend: {
-                  display: false,
-                },
-              },
-              scales: {
-                x: {
-                  grid: {
-                    drawOnChartArea: false,
-                  },
-                },
-                y: {
-                  ticks: {
-                    beginAtZero: true,
-                    maxTicksLimit: 5,
-                    stepSize: Math.ceil(250 / 5),
-                    max: 250,
-                  },
-                },
-              },
-              elements: {
-                line: {
-                  tension: 0.4,
-                },
-                point: {
-                  radius: 0,
-                  hitRadius: 10,
-                  hoverRadius: 4,
-                  hoverBorderWidth: 3,
-                },
-              },
-            }}
-          />
-        </CCardBody>
-        <CCardFooter>
-          <CRow xs={{ cols: 1 }} md={{ cols: 5 }} className="text-center">
-            {progressExample.map((item, index) => (
-              <CCol className="mb-sm-2 mb-0" key={index}>
-                <div className="text-medium-emphasis">{item.title}</div>
-                <strong>
-                  {item.value} ({item.percent}%)
-                </strong>
-                <CProgress thin className="mt-2" color={item.color} value={item.percent} />
-              </CCol>
-            ))}
-          </CRow>
-        </CCardFooter>
-      </CCard> */}
+            
 
       {/* <WidgetsBrand withCharts /> */}
 
       <CRow>
         {/* <CCol xs> */}
           <CCard className="mb-4">
-            {/* <CCardHeader>Traffic {' & '} Sales</CCardHeader> */}
-            {/* <CCardBody> */}
-              {/* <CRow> */}
-                {/* <CCol xs={12} md={6} xl={6}> */}
-                  {/* <CRow>
-                    <CCol sm={6}>
-                      <div className="border-start border-start-4 border-start-info py-1 px-3">
-                        <div className="text-medium-emphasis small">New Clients</div>
-                        <div className="fs-5 fw-semibold">9,123</div>
-                      </div>
-                    </CCol>
-                    <CCol sm={6}>
-                      <div className="border-start border-start-4 border-start-danger py-1 px-3 mb-3">
-                        <div className="text-medium-emphasis small">Recurring Clients</div>
-                        <div className="fs-5 fw-semibold">22,643</div>
-                      </div>
-                    </CCol>
-                  </CRow> */}
-
-                  {/* <hr className="mt-0" /> */}
-                  {/* {progressGroupExample1.map((item, index) => (
-                    <div className="progress-group mb-4" key={index}>
-                      <div className="progress-group-prepend">
-                        <span className="text-medium-emphasis small">{item.title}</span>
-                      </div>
-                      <div className="progress-group-bars">
-                        <CProgress thin color="info" value={item.value1} />
-                        <CProgress thin color="danger" value={item.value2} />
-                      </div>
-                    </div>
-                  ))} */}
-                  {/* {progressGroupExample2.map((item, index) => (
-                    <div className="progress-group mb-4" key={index}>
-                      <div className="progress-group-header">
-                        <CIcon className="me-2" icon={item.icon} size="lg" />
-                        <span>{item.title}</span>
-                        <span className="ms-auto fw-semibold">{item.value}%</span>
-                      </div>
-                      <div className="progress-group-bars">
-                        <CProgress thin color="warning" value={item.value} />
-                      </div>
-                    </div>
-                  ))} */}
-                {/* </CCol> */}
-
-                {/* <CCol xs={12} md={6} xl={6}>
-                  {/* <CRow>
-                    <CCol sm={6}>
-                      <div className="border-start border-start-4 border-start-warning py-1 px-3 mb-3">
-                        <div className="text-medium-emphasis small">Pageviews</div>
-                        <div className="fs-5 fw-semibold">78,623</div>
-                      </div>
-                    </CCol>
-                    <CCol sm={6}>
-                      <div className="border-start border-start-4 border-start-success py-1 px-3 mb-3">
-                        <div className="text-medium-emphasis small">Organic</div>
-                        <div className="fs-5 fw-semibold">49,123</div>
-                      </div>
-                    </CCol>
-                  </CRow> */}
-
-                {/* <hr className="mt-0" /> */}
+           
 <Container fluid className='bg-secondary '>
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f3f4f5" fill-opacity="0.5" d="M0,224L40,202.7C80,181,160,139,240,149.3C320,160,400,224,480,245.3C560,267,640,245,720,218.7C800,192,880,160,960,154.7C1040,149,1120,171,1200,165.3C1280,160,1360,128,1400,112L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>            
                 </Container>
@@ -359,9 +59,52 @@ const Dashboard = () => {
 
              
                 </Container>
-                <section className='bg-black '>
+                <section className=' profile-strength '>
 <h5 className='text-center ms-4 pt-5'>Complete your profile to become a Unive Superstar</h5>
- <p className='text-center fs-3 ms-3'>Basic  <HiOutlineMinus className='sv-hieght'/> <HiOutlineMinus className='sv-hieght'/> <HiOutlineMinus className='sv-hieght'/> <HiOutlineMinus className='sv-hieght'/> <HiOutlineMinus className='sv-hieght'/></p>
+ <p className='text-center fs-5 ms-3'>Basic  <HiOutlineMinus className='sv-hieght'/> <HiOutlineMinus className='sv-hieght'/> <HiOutlineMinus className='sv-hieght'/> <HiOutlineMinus className='sv-hieght'/> <HiOutlineMinus className='sv-hieght'/> <span> Super Star <AiTwotoneStar className='text-secondary'/> <AiTwotoneStar className='text-secondary'/> <AiTwotoneStar className='text-secondary'/> </span></p>
+                </section>
+                <section className=' profile-strength mt-5 '>
+<Row className=''>
+  <Col xs={12} md={6} className=''>
+    <div className=" d-flex justify-content-between align-items-center ">
+    <h6 className='ms-3'>About</h6>
+    <BsPencilSquare className=' d-block my-2 fs-3 border py-1  '/>
+    </div>
+    <div className='text-center mx-5 mt-5 mb-5 py-4 borderDotted text-secondary d-flex justify-content-center align-items-center'>
+    <div className=''>Tell others about yourself</div>
+     
+    </div>
+  </Col>
+  <Col xs={12} md={6} className=''>
+    <p className='my-2'>My Job Status</p>
+    <div className="w-75 mt-3">
+    <Dropdown className=''>
+      <Dropdown.Toggle variant="white" id="dropdown-basic" className='border border-dark '>
+      <ImCross className='text-warning me-2'/> Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1" className='border border-dark'> <div><h2>Action</h2>
+        <p>asdasdasd</p>
+        </div></Dropdown.Item>
+        <Dropdown.Item href="#/action-1" className='border border-dark'> <div><h2>Action</h2>
+        <p>asdasdasd</p>
+        </div></Dropdown.Item>
+        <Dropdown.Item href="#/action-1" className='border border-dark'> <div><h2>Action</h2>
+        <p>asdasdasd</p>
+        </div></Dropdown.Item>
+        
+      </Dropdown.Menu>
+    </Dropdown>
+              </div>
+        <p className='pt-3'>Unive Certifications</p>      
+    <div className='text-center mb-2 w-75 borderDotted text-secondary'>
+    <p className='pt-2  mt-3'> Get certified in your dream  role</p>
+    <p className='text-dark fw-bold text-center ms-2'>Visit Certification</p>
+
+    </div>
+              </Col>
+</Row>
                 </section>
                 {/* {!admin && <div className="mb-5">
                   <Container>
