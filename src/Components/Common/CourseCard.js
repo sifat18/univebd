2ba28dd6d5c71cc7e8225d06a09a4020b9498d10
sifat18 <ibnsifat@gmail.com>
2022-10-19 14:28:31 +0600

    card style update

--- src/Components/Common/CourseCard.js
+++ src/Components/Common/CourseCard.js
@@ -1,14 +1,21 @@
 import axios from 'axios'
+import { useState } from 'react'
 
 import { Button, Card, Col, Row } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 import useAuth from '../Context/useAuth'
 import ar from '../images/icons8-arrow-.png'
 import st from '../images/steps.png'
-
+import ReactCardFlip from "react-card-flip";
+const CardStyle = {
+    border: "1px solid black",
+    // padding: "20px",
+    margin: "20px",
+    height: "300px"
+  };
 export default function CourseCard({id}) {
     const { user, admin } = useAuth()
-
+    const [isFlipped, setIsFlipped] = useState(false);
     const deleteCourse = (id) => {
         const {_id,...rest}=id
         console.log(rest)
@@ -21,26 +28,59 @@ export default function CourseCard({id}) {
     }
     return (
         <Col key={id._id}>
-        <Card className='py-1 card-shadow'>
-            <Card.Img variant="top" className='img-fluid ' src={id.imageLink} />
-            <Card.Body className='text-start'>
-                <p>Unive</p>
-                <Card.Title >{id.coursename}</Card.Title>
-                <Card.Text>{id.about.slice(0, 100)}              </Card.Text>
+     <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <Card
+        style={CardStyle}
+        onMouseEnter={() => setIsFlipped((prev) => !prev)}
+        className="CardFront card-shadow border-0"
+      >
+        <Card.Img variant="top" className='img-fluid ' src={id.imageLink} />
+        <p className='jobText text-start ms-2'>Unive</p>
+                <Card.Title className='jobText fw-bold text-center text-decoration-underline'>{id.coursename}</Card.Title>
+      </Card>
+      <Card
+        style={CardStyle}
+        onMouseLeave={() => setIsFlipped((prev) => !prev)}
+        className="CardBack card-shadow border-0" >
+    <Card.Body className='text-start'>
+                <p className='jobText '>Unive</p>
+                <Card.Title className='jobText fw-bold'>{id.coursename}</Card.Title>
+                <Card.Text className='jobText'>{id.about.slice(0, 100)+'...'}              </Card.Text>
             </Card.Body>
             <Row>
                 <Col xs={admin ? 4 : 6}>
                     <p><img src={st} alt="" height={25} /></p>
-                    <p className='fs-7'>Beginner</p>
+                    <p className='fs-7 jobText'>Beginner</p>
                 </Col>
                 <Col xs={admin ? 8 : 6} className='d-flex justify-content-around'>
-                    <NavLink to={`/learn/${id.coursename}`}> <Button className='p-2' variant="outline-dark">Preview <img src={ar} alt="" /></Button></NavLink>
-                    {admin && <NavLink to={`/dashboard/edit/${id.coursename}`}> <Button className='p-2' variant="outline-success">Edit Course <img src={'ar'} alt="" /></Button></NavLink>}
+                    <NavLink to={`/learn/${id.coursename}`}> <Button className='p-1 jobText' variant="outline-dark">Preview <img src={ar} alt="" /></Button></NavLink>
+                    {admin && <NavLink to={`/dashboard/edit/${id.coursename}`}> <Button className='p-1 jobText' variant="outline-success">Edit Course <img src={'ar'} alt="" /></Button></NavLink>}
 
                 </Col>
             </Row>
-            {admin &&  <Card.Footer className="text-muted"><Button className='p-2' variant="outline-danger" onClick={()=>deleteCourse(id)}>Delete Course <img src={'ar'} alt="" /></Button></Card.Footer>}
-        </Card>
+            {admin &&  <Card.Footer className="text-muted"><Button className='p-1  jobText' variant="outline-danger" onClick={()=>deleteCourse(id)}>Delete Course <img src={'ar'} alt="" /></Button></Card.Footer>}
+      </Card>
+    </ReactCardFlip>
+        {/* <Card className=' card-shadow'>
+            <Card.Img variant="top" className='img-fluid ' src={id.imageLink} />
+            <Card.Body className='text-start'>
+                <p className='jobText '>Unive</p>
+                <Card.Title className='jobText fw-bold'>{id.coursename}</Card.Title>
+                <Card.Text className='jobText'>{id.about.slice(0, 100)}              </Card.Text>
+            </Card.Body>
+            <Row>
+                <Col xs={admin ? 4 : 6}>
+                    <p><img src={st} alt="" height={25} /></p>
+                    <p className='fs-7 jobText'>Beginner</p>
+                </Col>
+                <Col xs={admin ? 8 : 6} className='d-flex justify-content-around'>
+                    <NavLink to={`/learn/${id.coursename}`}> <Button className='p-1 jobText' variant="outline-dark">Preview <img src={ar} alt="" /></Button></NavLink>
+                    {admin && <NavLink to={`/dashboard/edit/${id.coursename}`}> <Button className='p-1 jobText' variant="outline-success">Edit Course <img src={'ar'} alt="" /></Button></NavLink>}
+
+                </Col>
+            </Row>
+            {admin &&  <Card.Footer className="text-muted"><Button className='p-1  jobText' variant="outline-danger" onClick={()=>deleteCourse(id)}>Delete Course <img src={'ar'} alt="" /></Button></Card.Footer>}
+        </Card> */}
     </Col>
     )
 }
